f3add13d6565a086c76f2a5b04ac31e54c773e31
DariaYashchuk <dariayashchuk2000@gmail.com>
2023-08-18 20:55:48 +0300

    v1

--- src/redux/contacts/contactsSlice.js
+++ src/redux/contacts/contactsSlice.js
@@ -15,57 +15,51 @@ const handleRejected = (state, action) => {
   state.error = action.payload;
 };
 
-export const contactsSlice = createSlice(
-  {
-    name: 'contacts',
-    initialState: contactsInitialState,
-    extraReducers: {
-      // ====== fetchContacts ====
-      [fetchContacts.pending]: handlePending,
-      [fetchContacts.rejected]: handleRejected,
-      [addContact.pending]: handlePending,
-      [addContact.rejected]: handleRejected,
-      [deleteContact.pending]: handlePending,
-      [deleteContact.rejected]: handleRejected,
-      //   state.isLoading = true;
-      // },
-      [fetchContacts.fulfilled](state, action) {
-        // return (state = {
-        //   ...state,
-        //   isLoading: false,
-        //   error: null,
-        //   items: action.payload,
-        // });
-        state.isLoading = false;
-        state.error = null;
-        state.items = action.payload;
-      },
+export const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: contactsInitialState,
+  extraReducers: {
+    [fetchContacts.pending]: handlePending,
+    [fetchContacts.rejected]: handleRejected,
+    [addContact.pending]: handlePending,
+    [addContact.rejected]: handleRejected,
+    [deleteContact.pending]: handlePending,
+    [deleteContact.rejected]: handleRejected,
 
-      // ====== Contact ====
+    // ====== fetchContacts ====
 
-      [addContact.fulfilled](state, action) {
-        state.isLoading = false;
-        state.error = null;
-        state.items = [action.payload, ...state.items];
-      },
+    [fetchContacts.fulfilled](state, action) {
+      state.isLoading = false;
+      state.error = null;
+      state.items = action.payload;
     },
+
+    // ====== Contact ====
+
+    [addContact.fulfilled](state, action) {
+      state.isLoading = false;
+      state.error = null;
+      state.items = [action.payload, ...state.items];
+    },
+
     // ====== deleteContact ====
+
     [deleteContact.fulfilled](state, action) {
       state.isLoading = false;
       state.error = null;
       state.items = state.items.filter(item => item.id !== action.payload);
     },
-  }
+  },
+});
 
-  // addContact({ items }, action) {
-  //   console.log(items);
-  //   items.push({ ...action.payload });
-  // },
-  // deleteContact({ items }, action) {
-  //   console.log(items);
-  //   return items.filter(item => item.id !== action.payload);
-  // },
-);
+// addContact({ items }, action) {
+//   console.log(items);
+//   items.push({ ...action.payload });
+// },
+// deleteContact({ items }, action) {
+//   console.log(items);
+//   return items.filter(item => item.id !== action.payload);
+// },
 
 // export const { fetchingInProgress, fetchingSuccess, fetchingError } =
 //   contactsSlice.actions;
